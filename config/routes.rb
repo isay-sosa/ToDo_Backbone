55cb7482d4f86cb5d7a3e3cb1f4edb041c84942a
Isay Sosa <yasnet21@gmail.com>
2014-04-15 10:40:12 -0500

    Adding todos controller. Adding route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TodoList::Application.routes.draw do
+  namespace :api do
+    resources :todos
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
