4c98fc697a88ec6d53751528f86cf62776265c70
Isay Sosa <yasnet21@gmail.com>
2014-04-15 10:26:50 -0500

    Initialize todo list app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TodoList::Application.config.secret_key_base = 'b5f2901b7b4d5f2a06e4d058bed3450eb21590c13f3567a2d32c180ed35328174cc85b3560ba9305fac285545d7088c8b8c8ebd26043669aab899b633ed77943'
